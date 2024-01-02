bf1cf908d77b245a5dee9727fb8efcfbbcc6ce0e
JSChan03 <147693936+JSChan03@users.noreply.github.com>
2024-01-02 16:31:21 +0800

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+ 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
