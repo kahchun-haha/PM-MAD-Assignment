a0b557a60ad2c567481df3f349b9297104ba924b
JSChan03 <147693936+JSChan03@users.noreply.github.com>
2024-01-02 16:32:16 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -10,7 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-
+ 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
